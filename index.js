0758537471bf16ac9c34b0b4a4e4eab474f82c02
manojnagaraj01 <n.manoj9535@gmail.com>
2024-02-13 14:09:05 +0530

    devbranch

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ const bodyparser = require('body-parser')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const app = express()
-const dotenv = require("dotenv")
+const dotenv = require('dotenv').config()
+
+
 const userAuth = require("./routes/userRoutes")
 
 
-dotenv.config()
+
 
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: ['*', 'https://m.stripe.network', 'https://js.stripe.com']
+    origin: ['*', 'https://m.stripe.network', 'https://js.stripe.com', 'https://stripe-payments-app.herokuapp.com','http://localhost:5173'],
   }));
 //database
 const {dbconnect} = require("./config/dbconnect")
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,11 @@
 const route = require("express").Router()
-const {signup, signin, createCheckOutSession} = require("../controllers/userController")
+const {signup, signin, checkout,offer,enquiryform,order} = require("../controllers/userController")
 
 
 route.post("/signup", signup)
 route.post("/signin", signin)
-route.post('/create-checkout-session',createCheckOutSession)
-// route.post("/create-checkout-session", checkout)
-// route.post("/getCourseOfUser", offer)
-// route.post("/sendEnquiry", enquiryform)
-// route.get("/order/success", order)
+route.post("/create-checkout-session", checkout)
+route.post("/getCourseOfUser", offer)
+route.post("/sendEnquiry", enquiryform)
+route.get("/order/success", order)
 module.exports = route;
